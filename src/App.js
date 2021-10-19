9e177b553317ec223360cf9d1ce07931e1c76957
Abhinav Namdeo <abhaynam22@gmail.com>
2021-10-19 14:28:11 +0530

    Making UI

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import HomePage from "./components/HomePage";
+import Navbar from "./components/Navbar";
+
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <HomePage/>
+    </>
+  );
+}
+
+export default App;

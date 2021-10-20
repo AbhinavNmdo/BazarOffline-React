a9e85270b56d374ff5229194a851f69124591a4b
Abhinav Namdeo <abhaynam22@gmail.com>
2021-10-20 14:17:36 +0530

    Completed Header

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import header from "../Images/header1.jpg";
+import header from "../Images/header7.png";
 import "../css/style.css";
 
 const HomePage = () => {
@@ -9,14 +9,39 @@ const HomePage = () => {
         <div className="container-fluid">
           <div className="row">
             <div className="col-md-6 ">
-              <div className="text d-flex flex-column justify-content-center align-items-start "><h1>BazarOffline</h1></div>
+              <div className="text d-flex flex-column justify-content-center align-items-start "><h1 style={{fontFamily: 'Kaushan Script, cursive', marginLeft: '75px'}}>BazarOffline</h1></div>
             </div>
             <div className="col-md-6">
-              <img src="https://previews.123rf.com/images/kikiabdurahman/kikiabdurahman2004/kikiabdurahman200400013/143721388-vector-illustration-of-online-shopping-e-commerce-and-retail-concept-modern-flat-web-landing-page-te.jpg" alt="" className="img-fluid img" style={{width: '95%'}} />
+              <img src={header} alt="" className="img-fluid img" style={{width: '95%'}} />
             </div>
           </div>
         </div>
       </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-3">
+            <div className="card" style={{borderRadius: '10px'}}>
+              <h1>asdlfkj</h1>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="card" style={{borderRadius: '10px'}}>
+              <h1>asdlfkj</h1>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="card" style={{borderRadius: '10px'}}>
+              <h1>asdlfkj</h1>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="card" style={{borderRadius: '10px'}}>
+              <h1>asdlfkj</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </>
   );
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,13 +2,13 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light" style={{backgroundColor: '#e86c0e', zIndex: '100'}} >
+    <nav className="navbar navbar-expand-lg navbar-light" style={{backgroundColor: '#0e83b5', zIndex: '100'}} >
       <div className="container-fluid">
-        <a className="navbar-brand" style={{fontSize: '1.5rem'}} href="/">
+        {/* <a className="navbar-brand" style={{fontSize: '1.5rem', fontFamily: 'Kaushan Script, cursive'}} href="/">
             <strong>
                 BazarOffline
             </strong>
-        </a>
+        </a> */}
         <button
           className="navbar-toggler"
           type="button"
@@ -20,8 +20,8 @@ const Navbar = () => {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <div className="collapse navbar-collapse" id="navbarSupportedContent" style={{marginLeft: '150px'}}>
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0 d-flex align-items-right">
             <li className="nav-item">
               <a className="nav-link active" aria-current="page" href="/">
                 Home
@@ -65,14 +65,14 @@ const Navbar = () => {
               </ul>
             </li>
           </ul>
-          <form className="d-flex">
+          <form className="d-flex" style={{marginRight: '200px'}}>
             <input
               className="form-control me-2"
               type="search"
               placeholder="Search"
               aria-label="Search"
             />
-            <button className="btn btn-outline-success" type="submit">
+            <button className="btn btn-outline-warning" type="submit">
               Search
             </button>
           </form>

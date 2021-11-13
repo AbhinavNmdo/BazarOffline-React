f40bc2889c69b30213f3a4779edd08f5135555b3
Abhinav Namdeo <abhaynam22@gmail.com>
2021-11-13 20:34:30 +0530

    Working Started Again

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import header from "../Images/header7.png";
+import "../css/style.css";
+// import Slider from "react-slick";
+
+const HomePage = () => {
+  return (
+    <>
+      <div className="container">
+        
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
